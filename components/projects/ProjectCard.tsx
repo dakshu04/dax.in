1593b5a1cfd5bd109715a769058a18c2ba6345ea
Daksh Purohit <dakshgrows@gmail.com>
2026-02-09 00:20:33 +0530

    remove-readmore-from-projects

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -145,12 +145,12 @@ export function ProjectCard({ project } : ProjectCardProps) {
                     </>
                     )}
                 </div>
-                <Link
+                {/* <Link
                     href={project.projectDetailsPageSlug}
                     className="text-secondary hover:text-primary flex items-center gap-2 text-sm underline-offset-4 transition-colors hover:underline"
                 >
                     View Details <ArrowRight className="size-4" />
-                </Link>
+                </Link> */}
                 </CardFooter>
             )}
         </Card>
